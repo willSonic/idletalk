11407520a9a568abbaed57e27dbb5a45d3c953ab
Jason Kulatunga <jk17@ualberta.ca>
2013-03-07 16:41:26 -0500

    request local video stream working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,7 +26,31 @@ function IdleTalkLoginCtrl($scope, $http) {
 
 
 function IdleTalkHomeCtrl($scope, $routeParams) {
+    var socket = new WebSocket('ws://124.169.31.194:1337/');
+    var sourcevid = document.getElementById('sourcevid');
+    var remotevid = document.getElementById('remotevid');
+    var localStream = null;
+    var peerConn = null;
+    var started = false;
+    
+    
   $scope.phoneId = $routeParams.phoneId;
+  $scope.shareVideo = function(){
+      // Replace the source of the video element with the stream from the camera
+      try { //try it with spec syntax
+        navigator.webkitGetUserMedia({audio: true, video: true}, successCallback, errorCallback);
+      } catch (e) {
+        navigator.webkitGetUserMedia("video,audio", successCallback, errorCallback);
+      }
+      function successCallback(stream) {
+          sourcevid.src = window.webkitURL.createObjectURL(stream);
+          localStream = stream;
+      }
+      function errorCallback(error) {
+          console.error('An error occurred: [CODE ' + error.code + ']');
+      }
+      
+  }
 }
 
 //PhoneDetailCtrl.$inject = ['$scope', '$routeParams'];
